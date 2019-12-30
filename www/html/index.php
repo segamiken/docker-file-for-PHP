ca98dc0646a475457c854e24f7d63d361b330cc7
Ken <ken.s,vollyball@gmail.com>
2019-12-30 17:47:22 +0900

    Fix html file

--- www/html/index.php
+++ www/html/index.php
@@ -7,10 +7,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Post Imgage App</title>
+    <title>Title</title>
 </head>
 <body>
-    <h1>hhhh</h1>
+
 </body>
 </html>
 
